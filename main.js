a73833250f01e131a76038dbd40b49e9b5f22ba6
Genaro Garcia <genaro.dev@outlook.com>
2022-10-31 14:09:37 -0500

    function to display books on their own card

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-let myLibrary = [];
-
 function Book(title, author, pages) {
   this.title = title;
   this.author = author;
@@ -11,3 +9,34 @@ const newBookBtn = document.querySelector('#newBook');
 newBookBtn.addEventListener('click', () => {
   document.querySelector('.form').style.display = 'block';
 });
+
+function newBook() {
+  let myLibrary = [
+    {
+      title: 'The Hobbit',
+      author: 'J.R.R Tolkien',
+      pages: 395,
+    },
+  ];
+
+  let books = myLibrary;
+
+  books.forEach((book) => addBookToCard(book));
+}
+
+// add book's info (title,author,pages) to card
+function addBookToCard(book) {
+  const container = document.querySelector('.container');
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  card.innerHTML = `
+  <div class='card-book-title'>${book.title}</div>
+  <div class='card-book-author'>Author: ${book.author}</div>
+  <div class='card-book-pages'>Pages: ${book.pages}</div>
+  `;
+
+  container.appendChild(card);
+}
+
+newBook();
